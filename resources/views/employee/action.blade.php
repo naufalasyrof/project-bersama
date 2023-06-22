57eb2e455069c63bfa9407815b93b071d3077b3e
RaihanFebrian01 <febrianadi2001@gmail.com>
2023-06-22 20:53:43 +0700

    edit action2

--- resources/views/employee/action.blade.php
+++ resources/views/employee/action.blade.php
@@ -1,3 +1,4 @@
+<!-- asoyyy ini action -->
 <div class="d-flex">
     <a href="{{ route('employees.show', ['employee' => $employee->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
     <a href="{{ route('employees.edit', ['employee' => $employee->id]) }}" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
